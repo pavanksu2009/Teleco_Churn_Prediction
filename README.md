692888c297f8fec6e4ae71416778b8ad9f20413e
pavanksu2009 <pavankumar.ch7@gmail.com>
2022-11-29 21:51:10 +0000

    This commit includes model notebooks and app file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Teleco_Churn_Prediction
+
+The app is deployed to Streamlit, and can be found at this 
+
+
+
+
+# Churn Modelling - How to predict if a Telecom’s customer will stay or leave.
+
+We created an app to demonstrate the ability to apply best machine learning model to predict the likelihood of customer churn. We accomplished this using the following steps:
+
+## 1. Clean the data
+
+By reading the dataset into a dataframe using pandas,  we removed unnecessary data field customer IDs. 
+
+## 2. Analyze initial DataFrame
+
+Utilizing Matplotlib, Seaborn and Pandas, we next analyzed the data.
+
+## 3. Machine Learning using 5 different models
+
+We tested five different machine learning models (and used  in the final application) to predict customer churn, including Logistic Regression, Decision Tree, Random Forest, K-Nearest Neighbor, and XGBoost. 
+
+## 4. Load model to display predictions on app
+
+Finally, using Streamlit, we created the user-facing app to add information to our data set matching our initial dataframe to predict the likelihood of a customer departing the company. This was then deployed to Streamlit,
+and can be found at this URL:
+ 
---
+++ churn_pred_app.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import joblib
+
+load_model = joblib.load(r"models\challa_logistic_regression.sav")
+
+def  main():
+    st.sidebar.info("This app is created to predict churn")
+    st.title("Predicting customer churn")
+    Gender = st.selectbox('Gender:', [0, 1])
+    Senior_Citizen= st.selectbox(' Customer is a senior citizen:', [0, 1])
+    Partner= st.selectbox(' Customer has a partner:', ['Yes', 'No'])
+    Dependents = st.selectbox(' Customer has  dependents:', ['Yes', 'No'])
+    Phone_Service = st.selectbox(' Customer has phoneservice:', ['Yes', 'No'])
+    Multiple_Lines = st.selectbox(' Customer has multiplelines:', ['Yes', 'No', 'no_phone_service'])
+    Internet_Service= st.selectbox(' Customer has internetservice:', ['DSL', 'No', 'Fiber optic'])
+    Online_Security= st.selectbox(' Customer has onlinesecurity:', ['Yes', 'No', 'No_internet_service'])
+    Online_Backup = st.selectbox(' Customer has onlinebackup:', ['Yes', 'No', 'No_internet_service'])
+    Device_Protection = st.selectbox(' Customer has deviceprotection:', ['Yes', 'No', 'No_internet_service'])
+    Tech_Support = st.selectbox(' Customer has techsupport:', ['Yes', 'No', 'No_internet_service'])
+    Streaming_TV = st.selectbox(' Customer has streamingtv:', ['Yes', 'No', 'No_internet_service'])
+    Streaming_Movies = st.selectbox(' Customer has streamingmovies:', ['Yes', 'No', 'No_internet_service'])
+    Contract= st.selectbox(' Customer has a contract:', ['Month-to-month', 'One year', 'Two year'])
+    Paperless_Billing = st.selectbox(' Customer has a paperlessbilling:', ['Yes', 'No'])
+    Payment_Method= st.selectbox('Payment Option:', ['Bank transfer (automatic)', 'Credit card (automatic)', 'Electronic check' ,'Mailed check'])
+    Tenure = st.number_input('Number of months the customer has been with the current telco provider :', min_value=0, max_value=240, value=0)
+    Monthly_Charges= st.number_input('Monthly charges :', min_value=0, max_value=240, value=0)
+    Total_Charges = Tenure*Monthly_Charges
+    output= ""
+    input_dict = {
+        "Gender":Gender ,
+		"Senior_Citizen": Senior_Citizen,
+		"Partner": Partner,
+		"Dependents": Dependents,
+		"Phone_Service": Phone_Service,
+		"Multiple_Lines": Multiple_Lines,
+		"Internet_Service": Internet_Service,
+		"Online_Security": Online_Security,
+		"Online_Backup": Online_Backup,
+		"Device_Protection": Device_Protection,
+		"Tech_Support": Tech_Support,
+		"Streaming_TV": Streaming_TV,
+		"Streaming_Movies": Streaming_Movies,
+		"Contract": Contract,
+		"Paperless_Billing": Paperless_Billing,
+		"Payment_Method": Payment_Method,
+		"Tenure": Tenure,
+		"Monthly_Charges": Monthly_Charges,
+		"Total_Charges": Total_Charges,
+		}
+    ok = st.button("Churn Prediction")
+    if ok:
+        X =  pd.DataFrame([input_dict])
+        y_pred = load_model.predict(X)
+        output = y_pred[0]
+        if output > 0.5:
+            st.write("The customer Exited")
+        else:
+            st.write("The customer Stayed")    
+            
+
+if __name__ == '__main__':
+	main()
